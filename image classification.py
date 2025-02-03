e5ef9f024ada8a0f4facf0855c99ff7ef8f4a698
Sri Ganesh <sriganesh8855@gmail.com>
2025-02-02 19:16:33 -0800

    added code to import and transform datsets

--- image classification.py
+++ image classification.py
@@ -1,6 +1,41 @@
 
-import tensorflow as tf
-from tensowrflow.keras import layers
-import pandas as pd
-from matplotlib import pyplot as plt
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torchvision.models as models
+
+# data transformations
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32,padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5)),
+])
+
+# data Loading
+train_dataset = torchvision.datasets.CIFAR10(
+    root='./data',
+    train=True,
+    download=True,
+    transform=transform_train
+)
+
+test_dataset = torchvision.datasets.CIFAR10(
+    root='./data',
+    train=False,
+    download=True,
+    transform=transform_test
+)
+
+# dataset loaders
+train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=32,shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=32,shuffle=False)
+
 
